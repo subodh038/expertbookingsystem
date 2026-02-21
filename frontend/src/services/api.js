82fd9a02a3994f3b8429be815372b88881e00fe9
Subodh Sharma <sharmsubodh038@gmail.com>
2026-02-21 14:31:05 +0530

    Use env variable for API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "https://expert-booking-backend.onrender.com"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default API;
